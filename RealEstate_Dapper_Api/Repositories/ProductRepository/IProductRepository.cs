776442a2739ce6d131ace7c7a1ac15559faa46b7
harunseki <harunseki@gmail.com>
2025-01-17 15:08:40 +0300

    ders 9

---
+++ RealEstate_Dapper_Api/Repositories/ProductRepository/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Dtos.ProductDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.ProductRepository
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllCategoryAsync();
+
+        Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync();
+
+        void CreateCategory(CreateProductDto categoryDto);
+
+        void UpdateCategory(UpdateProductDto categoryDto);
+
+        void DeleteCategory(int id);
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Dtos.ProductDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.ProductRepository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Context _context;
+        public ProductRepository(Context context)
+        {
+            _context = context;
+        }
+        public void CreateCategory(CreateProductDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultProductDto>> GetAllCategoryAsync()
+        {
+            string query = "SELECT * FROM Product";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync()
+        {
+            string query = "SELECT Product.*, CategoryName FROM Product INNER JOIN Category ON Product.ProductCategory=Category.CategoryID";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductWithCategoryDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public void UpdateCategory(UpdateProductDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
