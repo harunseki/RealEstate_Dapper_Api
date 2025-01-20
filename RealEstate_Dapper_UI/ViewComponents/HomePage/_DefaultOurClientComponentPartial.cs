b18847c52f558079149fa9d42448f54a3c498f6f
harunseki <harunseki@gmail.com>
2025-01-20 15:25:59 +0300

    desr 12

---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultOurClientComponentPartial.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Versioning;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultOurClientComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
